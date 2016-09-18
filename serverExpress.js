0e1a19d1d35d50ba634c2b6bb8300eca183bbc38
Jeannie Yamakawa <jkyamakawa@gmail.com>
2016-09-18 17:21:28 -0500

    Send individual guest records from the database

--- serverExpress.js
+++ serverExpress.js
@@ -3,7 +3,6 @@
 var fs = require('fs');
 var path = require('path');
 var guestsPath = path.join(__dirname, 'guests.json');
-
 var express = require('express');
 var app = express();
 var port = process.env.PORT || 8000;
@@ -11,15 +10,34 @@ var port = process.env.PORT || 8000;
 app.disable('x-powered-by');
 
 app.get('/guests', function(req, res) {
-  fs.readFile(guestsPath, 'utf8', function(err, guestsJSON) {
+  fs.readFile(guestsPath, 'utf8', function(err, data) {
     if (err) {
       console.error(err.stack);
       return res.sendStatus(500);
     }
 
-    var guests = JSON.parse(guestsJSON);
+    var guests = JSON.parse(data);
 
     res.send(guests);
+  });
+});
+
+app.get('/guests/:id', function(req, res) {
+  fs.readFile(guestsPath, 'utf8', function(err, data){
+    if (err == true) {
+      console.error(err.stack);
+      return res.sendStatus(500);
+    }
+
+    var id = Number.parseInt(req.params.id);
+    var guests = JSON.parse(data);
+
+    if (id < 0 || id >= guests.length || Number.isNaN(id)) {
+      return res.sendStatus(404);
+    }
+
+    res.set('Content-Type', 'text/plain');
+    res.send(guests[id]);
   });
 });
 
